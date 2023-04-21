95cfab612643ba04f3c67b1dd386b35aa78db037
demetrigeras <demetrijgeras@gmail.com>
2023-04-21 13:54:55 -0400

    one

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+ const getRandomRecipies = () => {
+ fetch("https://www.themealdb.com/api/json/v1/1/random.php") 
+    .then((res) => res.json())
+    .then ((res) => {
+    setRecipe(res.meals[0].strMeal)
+    setInstructions(res.meals[0].strInstructions)
+  })
+}
+  useEffect(() => getRandomRecipies,[])
+
+  const [recipe, setRecipe] = useState('')
+  
+
+  const [instructions, setInstructions] = useState('')
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <header>Random Food Recipes</header>
+      <div class="slide-show"></div>
+      <div className="meal">{recipe}</div>
+    <div class="recipe">{instructions}</div>
+   
+    <button onClick={getRandomRecipies} class="next">Next</button>
     </div>
   );
 }
